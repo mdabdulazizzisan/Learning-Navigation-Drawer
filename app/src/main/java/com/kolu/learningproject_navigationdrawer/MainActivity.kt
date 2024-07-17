9037fe34b1d74ee8b152f59819a129011c287c2b
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-07-17 10:46:51 +0600

    added view binding

--- app/src/main/java/com/kolu/learningproject_navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/kolu/learningproject_navigationdrawer/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kolu.learningproject_navigationdrawer.databinding.ActivityMainBinding
+import com.kolu.learningproject_navigationdrawer.databinding.ActivityMainContentBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bndActivityMainContent: ActivityMainContentBinding
+    lateinit var bndActivityMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bndActivityMainContent = ActivityMainContentBinding.inflate(layoutInflater)
+        bndActivityMain = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(bndActivityMain.root)
+        ViewCompat.setOnApplyWindowInsetsListener(bndActivityMain.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
