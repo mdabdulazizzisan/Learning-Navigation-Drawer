6215f8c13a72dd6db01736d7e4219668a1d7e33c
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-07-17 21:43:14 +0600

    click functionalities added

--- app/src/main/java/com/kolu/learningproject_navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/kolu/learningproject_navigationdrawer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kolu.learningproject_navigationdrawer
 
+import android.media.Image
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,19 +12,49 @@ import com.kolu.learningproject_navigationdrawer.databinding.ActivityMainContent
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var bndActivityMainContent: ActivityMainContentBinding
     lateinit var bndActivityMain: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bndActivityMainContent = ActivityMainContentBinding.inflate(layoutInflater)
         bndActivityMain = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(bndActivityMain.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(bndActivityMain.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<ImageView>(R.id.icon_menu).setOnClickListener {
+            bndActivityMain.contentInclude.iconMenu.setOnClickListener {
+                bndActivityMain.main.open()
+            }
+        }
+
+        bndActivityMain.navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.mProfile -> {
+                    Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                    bndActivityMain.main.close()
+                    true
+                }
+                R.id.mCart -> {
+                    Toast.makeText(this, "Cart clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.mHistory -> {
+                    Toast.makeText(this, "History clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.mSettings -> {
+                    Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    Toast.makeText(this, "Other item clicked", Toast.LENGTH_LONG).show()
+                    true
+                }
+            }
         }
     }
 }
